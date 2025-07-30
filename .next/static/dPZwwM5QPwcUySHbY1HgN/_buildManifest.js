dba882a27f79709dc407f80b00d49368c8f9f7f1
mml555 <mendel1023@gmail.com>
2025-07-30 09:23:40 -0400

    Remove .next build files from git tracking

--- .next/static/dPZwwM5QPwcUySHbY1HgN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-17719ad8abc06088.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
